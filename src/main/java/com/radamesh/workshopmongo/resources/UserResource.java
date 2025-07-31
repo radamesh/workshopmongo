e4650e3204a14dca94aeb20e2686782e06e7abfe
Radamés Silva <radamesh@gmail.com>
2025-07-31 18:19:35 -0300

    colocando delete e update na chamada

--- src/main/java/com/radamesh/workshopmongo/resources/UserResource.java
+++ src/main/java/com/radamesh/workshopmongo/resources/UserResource.java
@@ -1,25 +1,57 @@
 package com.radamesh.workshopmongo.resources;
 
 import com.radamesh.workshopmongo.domain.User;
+import com.radamesh.workshopmongo.dto.UserDTO;
 import com.radamesh.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
     private UserService service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> userList = service.findAll();
-        return ResponseEntity.ok().body(userList);
+        List<UserDTO> userDto = userList.stream().map(UserDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(userDto);
     }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(user));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insertUser(@RequestBody UserDTO newUser) {
+        User objUser = service.fromDTO(newUser);
+        objUser = service.insert(objUser);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(objUser.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@PathVariable String id, @RequestBody UserDTO newUserDto) {
+        User objUser = service.fromDTO(newUserDto);
+        objUser.setId(id);
+        service.update(objUser);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/com/radamesh/workshopmongo/services/UserService.java
+++ src/main/java/com/radamesh/workshopmongo/services/UserService.java
@@ -1,11 +1,14 @@
 package com.radamesh.workshopmongo.services;
 
 import com.radamesh.workshopmongo.domain.User;
+import com.radamesh.workshopmongo.dto.UserDTO;
 import com.radamesh.workshopmongo.repository.UserRepository;
+import com.radamesh.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +18,37 @@ public class UserService {
 
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty()) {
+            throw new ObjectNotFoundException("Este Usuário não foi encontrado.");
+        }
+        return user.orElse(null);
+    }
+
+    public User insert(User user) {
+        return userRepository.insert(user);
+    }
+    
+    public void delete(String id) {
+        findById(id);
+        userRepository.delete(id);
+    }
+    
+    public User update(User obj) {
+        User newObj = userRepository.findById(obj.getId());
+        updateData(newObj, obj);
+        return userRepository.save(newObj);
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setNome(obj.getNome());
+        newObj.setEmail(obj.getEmail());
+    }
+
+    public User fromDTO(UserDTO usrDto) {
+        return new User(usrDto.getId(), usrDto.getNome(), usrDto.getEmail());
     }
 }
